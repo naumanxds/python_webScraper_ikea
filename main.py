3c893f381004eeddfe3bd8ac82d092673de31121
nauman-coeus <nauman.nasir@coeus-solutions.de>
2019-11-11 23:33:53 +0500

    updates using selenium

--- main.py
+++ main.py
@@ -1,70 +1,140 @@
-# import requests
-# import requests_html
-# import time
-# import csv
+import time
+import csv
 
-# from datetime import datetime
-# from requests_html import HTMLSession
-# from bs4 import BeautifulSoup
-
-# # constants used in code
-# BASE_URL = 'https://www.amazon.ae'
-# NOT_FOUND = 'None'
-# INCREMENT_ONE = 1
-# SLEEP_SEC = 5
-
-
-# # create file with time attached to it for safty purposes
-# # fHandle = open('csvFileCreatedAt-' + datetime.now().strftime('%Y-%m-%d_%H:%M:%S') + '.csv', 'w', encoding="utf-8")
-
-
-# # get html of the provided url page
-# def getHtml(url):
-#     try:
-#         session = HTMLSession()
-#         response = session.get(url)
-#         return BeautifulSoup(response.text, 'lxml')
-#     except Exception as e:
-#         print('Oops! Something went worng fetching the link - ' + format(e))
-#     return false
-
-# # iterate through the fetched links get price and place in the file
-# def iterateLinks(subLinks):
-#     data = []
-#     html = getHtml(subLinks)
-#     try:
-#         # title of the product
-#         title = html.find('span', {'class' : 'normal-font range__text-rtlmmm'})
-#         if str(title) != NOT_FOUND:
-#             title = title.get()
-#         else:
-#             title = 'Title not Found'
-
-
-
-#     except:
-#         true
-
-# # input for user
-# enteredUrl = input('Please Enter Starting Point for Scrapper: ')
-# print('=== Starting Scrapping ===')
-# # iterateLinks(enteredUrl)
-# html = getHtml(enteredUrl)
-# print(html.find('div', {'class' : 'range-stock-check__left'}))
-
+from datetime import datetime
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
-url = 'https://www.ikea.com/ae/en/p/fullen-wash-basin-cabinet-white-70189041/'
+# constants used in code
+BASE_URL = 'https://www.amazon.ae'
+NOT_FOUND = 'None'
+INCREMENT_ONE = 1
+SLEEP_SEC = 1
 
+# create file with time attached to it for safty purposes
+fHandle = open('csvFileCreatedAt-' + datetime.now().strftime('%Y-%m-%d_%H:%M:%S') + '.csv', 'w', encoding="utf-8")
+
+# create browser instance
 options = webdriver.ChromeOptions();
 options.add_argument('--headless');
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-driver.get(url)
 
-chrome = driver.execute_script('return document.documentElement.outerHTML')
-html = BeautifulSoup(chrome, 'lxml')
-print(html.find_all('option', {'value' : '218'}))
+# get html of the provided page url
+def getHtml(url):
+    try:
+        driver.get(url)
+        html = driver.execute_script('return document.documentElement.outerHTML')
+        return BeautifulSoup(html, 'lxml')
 
+    except Exception as e:
+        print('     >> Error in Fetching HTML from Url => ' + url)
+        print('     >> ERRROR => ' + format(e))
+    return False
 
+# write in file
+def writeFile(data, url = ''):
+    try:
+        csvWriter = csv.writer(fHandle)
+        csvWriter.writerow(data)
+    except Exception as e:
+        print('     >> Error in Writing Data into the file => ' + url)
+        print('     >> ERRROR => ' + format(e))
+
+# iterate through the fetched links get data and place in the file
+def iterateLinks(subLinks):
+    for l in subLinks:
+        html = getHtml(l)
+        if html != False:
+            try:
+                # check the availibility of product in dubai
+                availibility = html.find('option', {'value' : '218'})
+                if str(availibility) == NOT_FOUND:
+                    continue
+
+                # title of the product
+                title = html.find('span', {'class' : 'normal-font range__text-rtl'})
+                if str(title) != NOT_FOUND:
+                    title = title.get_text(strip=True)
+                else:
+                    title = 'Title not Found'
+
+                price = html.find('span', {'class' : 'product-pip__price__value'})
+                if str(price) != NOT_FOUND:
+                    price = price.get_text().split('Dhs ')[1]
+                else:
+                    price = 'Price not Found'
+
+                description = html.find('div', {'id' : 'pip_product_description'})
+                if str(description) != NOT_FOUND:
+                    description = description.get_text(strip=True, separator='\n')
+                else:
+                    description = 'Description Not Found'
+
+                careInstructions = html.find('div', {'id' : 'pip_care_instructions'})
+                if str(careInstructions) != NOT_FOUND:
+                    careInstructions = careInstructions.get_text(strip=True)
+                else:
+                    careInstructions = 'Care Instructions Not Found'
+
+                environmentMaterial = html.find('div', {'id' : 'pip_environment_and_material'})
+                if str(environmentMaterial) != NOT_FOUND:
+                    environmentMaterial = environmentMaterial.get_text(strip=True, separator='\n')
+                else:
+                    environmentMaterial = 'Environment and Material Not Found'
+
+                size = html.find('div', {'id' : 'pip_dimensions'})
+                if str(size) != NOT_FOUND:
+                    size = size.get_text(strip=True, separator='\n')
+                else:
+                    size = 'Size Not Found'
+
+                pictures = html.find_all('li', {'class' : 'range-carousel-bullets__bullet js-range-carousel-bullets__bullet'})
+                picArr = []
+                if str(pictures) != NOT_FOUND:
+                    for pic in pictures:
+                        picArr.append(pic.find('img').get('src'))
+                else:
+                    picArr = ['Pictures Not Found']
+
+                writeFile([title, price, description, careInstructions, environmentMaterial, size] + picArr, subLinks)
+                print('     == Link Done => ' + l)
+                time.sleep(SLEEP_SEC)
+            except:
+                print('     >> Error in Fetching Data from Url => ' + subLinks)
+                print('     >> ERRROR => ' + format(e))
+
+# input for user
+enteredUrl = input('Please Enter Starting Point for Scrapper: ')
+enteredUrl = enteredUrl.split('/?page=')[0]
+print('=== Starting Scrapping ===')
+count = 1
+while count <= 10:
+    html = getHtml(enteredUrl + '/?page=' + str(count))
+    paginator = html.find('div', {'class' : 'pagination pagination--center'})
+    if str(paginator) == NOT_FOUND:
+        break
+
+    count += INCREMENT_ONE
+
+print('=== Fetching Link for All Products ===')
+divs = getHtml(enteredUrl + '/?page=' + str(count))
+divs = divs.find_all('div' , {'class' : 'product-compact__spacer'})
+links = []
+for l in divs:
+    links.append(l.find('a').get('href'))
+
+# place heading in the file
+writeFile([
+    'Title',
+    'Price',
+    'Description',
+    'Care Instructions',
+    'Environment Material',
+    'Size',
+    'Pictures'
+])
+
+iterateLinks(links)
+fHandle.close()
+print('=== Scrapping Finished ===')
